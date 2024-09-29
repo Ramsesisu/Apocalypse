f3f972faa58c47d92cfe3120643e2fbad7480d2f
rqmses <dy07urij@studserv.uni-leipzig.de>
2024-09-29 08:47:21 +0200

    Changes by janni

--- src/main/java/org/apocalypse/api/weapon/types/WeaponType.java
+++ src/main/java/org/apocalypse/api/weapon/types/WeaponType.java
@@ -2,6 +2,7 @@ package org.apocalypse.api.weapon.types;
 
 import lombok.Getter;
 import org.apocalypse.api.weapon.Weapon;
+import org.apocalypse.api.weapon.projectile.types.BulletType;
 import org.bukkit.Material;
 
 @Getter
@@ -10,17 +11,19 @@ public class WeaponType {
     private final String name;
     private final Material item;
     private final Weapon.Type type;
+    private final BulletType bullet;
+    private final double damage;
     private final int ammo;
     private final int magazine;
-    private final int damage;
 
-    public WeaponType(String name, Material item, int damage, int ammo, int magazine) {
+    public WeaponType(String name, Material item, BulletType bullet, int ammo, int magazine) {
         this.item = item;
         this.type = Weapon.Type.GUN;
         this.name = name;
         this.ammo = ammo;
         this.magazine = magazine;
-        this.damage = damage;
+        this.bullet = bullet;
+        this.damage = bullet.getDamage();
     }
 
     public WeaponType(String name, Material item, int damage) {
